1302e82f309b471ddb7b8dae628002e5d01ca465
jerryzhangjl <15824951217@139.com>
2018-06-19 19:02:00 +0800

    测试

--- src/test/java/example/OneTest.java
+++ src/test/java/example/OneTest.java
@@ -8,6 +8,7 @@ public class OneTest {
   public void testFoo() throws Exception {
     One one = new One();
     //Test foo
+    System.out.println("111");
     assertEquals("foo", one.foo());
   }
 
