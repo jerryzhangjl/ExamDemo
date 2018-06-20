5199723442de3b93236e4041df3be61f304d2396
jerryzhangjl <15824951217@139.com>
2018-06-20 20:16:22 +0800

    tijiao

--- src/main/java/com/migu/schedule/Schedule.java
+++ src/main/java/com/migu/schedule/Schedule.java
@@ -4,45 +4,130 @@ package com.migu.schedule;
 import com.migu.schedule.constants.ReturnCodeKeys;
 import com.migu.schedule.info.TaskInfo;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /*
 *类名和方法不能修改
  */
 public class Schedule {
-
+	
+	//注册服务列表 nodeId ,taskid列表
+	Map<Integer, List<Integer>> register = new HashMap<Integer, List<Integer>>();
+	//任务队列 taskid
+	List<Integer> queue = new LinkedList<Integer>();
+	// taskid和资源消耗率
+	Map<Integer, Integer> csInfo = new HashMap<Integer, Integer>();
 
     public int init() {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+    	register = new HashMap<Integer, List<Integer>>();
+    	queue = new LinkedList<Integer>();
+    	csInfo = new HashMap<Integer, Integer>();
+        return ReturnCodeKeys.E001;
     }
 
 
     public int registerNode(int nodeId) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+    	if(nodeId<=0)
+    	{
+    		return ReturnCodeKeys.E004;
+    	}
+    	if(register.containsKey(nodeId))
+    	{
+    		return ReturnCodeKeys.E005;
+    	}
+    	List<Integer> list = new ArrayList<Integer>();
+    	register.put(nodeId, list);
+    	return ReturnCodeKeys.E003;
     }
 
     public int unregisterNode(int nodeId) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+    	if(nodeId<=0)
+    	{
+    		return ReturnCodeKeys.E004;
+    	}
+    	if(!register.containsKey(nodeId))
+    	{
+    		return ReturnCodeKeys.E007;
+    	}
+    	List<Integer> list = register.get(nodeId);
+    	if(null != list && list.size()>0)
+    	{
+    		for(int i=0;i<list.size();i++)
+    		{
+    			queue.add(list.get(i));
+    		}
+    	}
+    	return ReturnCodeKeys.E006;
     }
 
 
     public int addTask(int taskId, int consumption) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+    	if(taskId<=0)
+    	{
+    		return ReturnCodeKeys.E009;
+    	}
+    	if(queue.contains(taskId))
+    	{
+    		return ReturnCodeKeys.E010;
+    	}
+    	queue.add(taskId);
+    	csInfo.put(taskId, consumption);
+        return ReturnCodeKeys.E008;
     }
 
 
     public int deleteTask(int taskId) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+    	if(taskId<=0)
+    	{
+    		return ReturnCodeKeys.E009;
+    	}
+    	if(!queue.contains(taskId))
+    	{
+    		return ReturnCodeKeys.E012;
+    	}
+    	queue.remove(Integer.valueOf(taskId));
+    	if(null !=register && register.size()>0)
+    	{
+    		 for (Map.Entry<Integer, List<Integer>> entry : register.entrySet()) {  
+    			   List<Integer> values = entry.getValue();  
+    			   for(Integer tInfo : values)
+    			   {
+    				   if(tInfo == taskId)
+    				   {
+    					   values.remove(tInfo);
+    				   }
+    			   }
+    			  }  
+    	}
+        return ReturnCodeKeys.E011;
     }
 
 
     public int scheduleTask(int threshold) {
-        // TODO 方法未实现
+    	if(threshold<=0)
+    	{
+    		return ReturnCodeKeys.E002;
+    	}
+    	// 计算注册服务器数量
+    	int nodesize = register.size();
+    	// 计算任务数量
+    	int quesize = queue.size();
+    	List<Integer> list = new ArrayList<Integer>();
+    	if(quesize > 0)
+    	{
+    		for(int note : queue)
+    		{	
+    			//占用率
+    			int rate = csInfo.get(note);
+    			list.add(rate);
+    		}
+    	}
+    	
+    	
         return ReturnCodeKeys.E000;
     }
 
---
+++ src/main/java/com/migu/schedule/info/ConsumptionInfo.java
@@ -0,0 +1,27 @@
+package com.migu.schedule.info;
+/**
+ * 资源消耗
+ * @author Administrator
+ *
+ */
+public class ConsumptionInfo {
+	private int taskId;
+	private int consumption;
+
+	public int getTaskId() {
+		return taskId;
+	}
+
+	public void setTaskId(int taskId) {
+		this.taskId = taskId;
+	}
+
+	public int getConsumption() {
+		return consumption;
+	}
+
+	public void setConsumption(int consumption) {
+		this.consumption = consumption;
+	}
+
+}
